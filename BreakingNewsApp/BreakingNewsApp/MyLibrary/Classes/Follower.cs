b7b20fbd469d0e16635f9c80234b5bb83ccf3d1f
Petrovski Martin <46264075+petrovskiq@users.noreply.github.com>
2019-04-09 04:28:26 -0700

    Up

---
+++ BreakingNewsApp/BreakingNewsApp/MyLibrary/Classes/Follower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary.Classes;
+using static MyLibrary.Classes.Medium;
+
+namespace MyLibrary.Classes
+{
+    public class Follower
+    {
+        public string Name { get; set; }
+
+        public Follower(string name)
+        {
+            Name = name;
+        }
+
+        public void Subscribe(Medium medium)
+        {
+            medium.EventBreakingNews += new Medium.NewsDelegate(PrintNews);
+        }
+
+        public void PrintNews(object obj, NewsEventArgs e)
+        {
+            Console.WriteLine($"{e.Title} {e.Content} {e.Date}");
+        }
+    }
+}
---
+++ BreakingNewsApp/BreakingNewsApp/MyLibrary/Classes/Medium.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Classes
+{
+    public class Medium
+    {
+        public delegate void NewsDelegate(object sender, NewsEventArgs e);
+
+        public event NewsDelegate EventBreakingNews;
+
+        public void SendBreakingNews(string title, string content, DateTime date)
+        {
+            NewsEventArgs News = new NewsEventArgs(title, content, date);
+            if (EventBreakingNews == null)
+            {
+                Console.WriteLine("No subscribers");
+                return;
+            }
+            EventBreakingNews(this, News);
+
+        }
+
+        public class NewsEventArgs : EventArgs
+        {
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public DateTime Date { get; set; }
+
+            public NewsEventArgs(string title, string content, DateTime date)
+            {
+                Title = title;
+                Content = content;
+                Date = date;
+            }
+        }
+    }
+}
